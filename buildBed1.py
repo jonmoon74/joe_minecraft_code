14ef5c6889ed21969aebec513583b4c4bbb4002f
jonmoon74 <jon.moon@outlook.com>
2018-03-03 15:32:32 +0000

    corrected typo

--- buildBed1.py
+++ buildBed1.py
@@ -21,8 +21,8 @@ def buildbed(wide, sheet, pillow):
 		mc.setBlock(x+1, y, z, wool, sheet)
 		mc.setBlock(x+2, y, z, wool, sheet)
 		mc.setBlock(x+3, y, z, wool, pillow)
-		mc.setblock(x+1, y, z+1, wool, sheet)
-		mc.setblock(x+2, y, z+1, wool, sheet)
+		mc.setBlock(x+1, y, z+1, wool, sheet)
+		mc.setBlock(x+2, y, z+1, wool, sheet)
 		mc.setBlock(x+3, y, z+1, wool, pillow)
 	else:
 		mc.postToChat("You didn't build a bed")
