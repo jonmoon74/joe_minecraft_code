92b5bd875f0d8939e5a7a869caf7614e063a9a43
jonmoon74 <jon.moon@outlook.com>
2018-07-01 15:27:55 +0000

    new bridge Builder file added to build custom length bridges

---
+++ bridgeBuilder.py
@@ -0,0 +1,48 @@
+def buildBridge(bridgeLength, material):
+    
+    #NEED TO PUT A EVEN NUMBER CHECKER FOR brigdeLength
+    if bridgeLength % 2 = 0:  
+        #import modules
+        from mcpi import minecraft
+        from mcpi import block
+        #get player co-ordinates
+        mc = minecraft.Minecraft.create()
+        pos = mc.player.getPos()
+        x, y, z = mc.player.getPos()
+        #set dictionary for block types
+        wool = block.WOOL.id
+        brick = block.BRICK_BLOCK.id
+        wood = block.WOOD_PLANKS.id
+        stone = block.STONE.id
+        #establish key variables for looping
+        i = 2
+        iTwo = 0
+        iThree = bridgeLength/2
+        iFour = iThree-1
+        iFive = iThree+1
+        #build the ascending half of the bridge
+        mc. setBlock(x+1, y, z, material)
+        while i < iFive:
+            #set a block in front of Steve
+            mc.setBlock(x+i, y, z+iTwo, material)
+            iTwo++
+            mc.setBlock(x+i, y, z+iTwo, material)
+            i++
+        #build the middle of the bridge   
+        mc.setBlock(x+iFive, y, z+iFour, material)
+        iFour--
+        mc.setBlock(x+iFive, y, z+iFour, material)
+        iFive++
+        #build the descending half of the bridge
+        while iFive < bridgeLength:
+            mc.setBlock(x+iFive, y, z+iFour, material)
+            iFour--
+            mc.setBlock(x+iFive, y, z+iFour, material)
+            iFive++
+        #build the final step down   
+        mc.setBlock(x+bridgeLength, y, z, material)
+    else:
+        print("You must use a even number for your bridge")
+        
+
+
